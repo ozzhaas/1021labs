6adbbaf0ebb18b310172da2742792f049c722ef1
ozzhaas <mkhaas@clemson.edu>
2020-01-23 10:54:39 -0500

    1.23 10:54am

--- lab2/stddev.h
+++ lab2/stddev.h
@@ -5,6 +5,11 @@
 #include <math.h>
 #include <stdio.h>
 
+/*This function will take in an array of floating point numbers
+and an integer that's value is the number of floating point
+numbers in the array. It will then use these two variables
+to compute the standard deviation of the numbers in the array.*/
+
     void stats (float* data, int n);
 
 
