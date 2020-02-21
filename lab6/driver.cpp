e3f751f392979c7665b557a606aeb0975088230c
ozzhaas <mkhaas@clemson.edu>
2020-02-21 13:57:15 -0500

    2.21 1:57pm

--- lab6/driver.cpp
+++ lab6/driver.cpp
@@ -16,13 +16,15 @@ int main () {
     string ownName = "default";
     string dogName = "default";
     string dogBreed = "default";
-    int dogAge = 0;
+    string dogAge = "0";
 
 
     cout << "\t\t\t~~~Dog Directory~~~\n";
     cout << "Type 1 to enter dog information or 2 to print information and quit.\n";
     cin >> userNum;
     vector <Dog> vec_dogs;
+    vector <string> info;
+    string line;
     while (userNum == 1) {
         cout << "Enter your name: \n";
         // cin >> ownName;
@@ -30,9 +32,15 @@ int main () {
         getline(cin, ownName);
         cout << "\nEnter you age: \n";
         cin >> ownAge;
-        cout << "\nEnter your dog's name, breed and age (Bella Spaniel 3): \n";
-        cin >> dogName >> dogBreed >> dogAge;
-        Dog mainDog(dogName, dogBreed, dogAge, ownName, ownAge);
+        cout << "\nEnter your dog's name, breed and age (Bella, Brittany Spaniel, 3): \n";
+        cin.ignore();
+        getline(cin, line);
+        stringstream lineStream(line);
+        getline(lineStream, dogName, ',');
+        getline(lineStream, dogBreed, ',');
+        getline(lineStream, dogAge, ',');
+        int dogsAge = stoi(dogAge);
+        Dog mainDog(dogName, dogBreed, dogsAge, ownName, ownAge);
         vec_dogs.push_back(mainDog);
         cout << "\n\nType 1 to enter dog information or 2 to print information and quit.\n";
         cin >> userNum;
