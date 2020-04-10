97f9bd04dce0eec2abdfed631cf0852e31ebf2a7
ozzhaas <kellenhaas@gmail.com>
2020-04-10 17:10:21 -0400

    4.10 5:10pm

--- lab11/zoo.cpp
+++ lab11/zoo.cpp
@@ -10,8 +10,6 @@
 using namespace std;
 
 int main() {
-
-    //Create the vector of Animal Objects
     vector<Animal*> myAnimals;
 
 
@@ -21,20 +19,18 @@ int main() {
     myAnimals.push_back(t1);
     myAnimals.push_back(t2);
 
-
     //Create two Monkey objects
+
     Animal* m1 = new Monkey("King Louie", "Eeee eee ooohaha", 5, 95.7);
-    Animal* m2 = new Monkey("Abu", "Eeek aaak oook", 12, 25.4);
+    Animal* m2 = new Monkey("Abu", "Eeek aaak oook", 12, 79.3);
     myAnimals.push_back(m1);
     myAnimals.push_back(m2);
 
-
-    cout << endl << "\tWelcome to the Jungle!" << endl;
-
-    //Iterate through the vector of pointers to Animal objects
-    //and print out all of the values stored
     vector<Animal*>::iterator iter;
-    for (iter = myAnimals.begin(); iter!= myAnimals.end(); iter++) {
+
+    cout << "\tWelcome to the Jungle!\n";
+
+    for(iter = myAnimals.begin(); iter != myAnimals.end(); iter++) {
         (*iter)->printInfo();
     }
 
